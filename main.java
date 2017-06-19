ef2f092ca4970109e4a3dea70fa9f105a7e64702
Antek <antek.rzad76@gmail.com>
2017-06-19 18:45:13 +0200

    Pierwszy commit

---
+++ main.java
@@ -0,0 +1,58 @@
+public class main
+{
+    public static void main()
+    {
+        System.out.println("Hello World!!!");//to jest wypisanie na ekran
+        System.out.println("Hello World!!!");
+        final double pi=3.1415;//to jest stała
+
+        /*
+        int x=2; // liczby całkowite
+        short xx = 1; // liczby całkowite
+        double d=3.14; //liczby zmienno przecinkowe z dokładnością 16 miejsc po przecinku
+        float f=3.14f; //liczby zmienne przecinkowe z dokładnościa 8 miejsc po przecinku
+        String s="Antek";//string jest łańsuchem znaków przechowuje litery liczby incaczej tekst
+        char litera='S'; //jedna litera/znak
+
+        boolean flaga=true;//typ logiczny przyjmuje wartości prawda/fałsz
+         */
+
+        int x=35;
+        int y=64;
+        if(x==y)
+        {
+            System.out.println("x i y mają równą wartość");
+        }
+        else
+        {
+            if(x>y)
+            {
+                System.out.println("x jest większy od y");
+            }
+            else
+            {
+                System.out.println("Y jest większy od x");
+            }
+        }
+
+        
+    int dzien=0;
+if(dzien==1)
+        {
+            System.out.println("poniedziałek");
+       
+        }
+        if(dzien==2)
+        
+             System.out.println("wtorek");
+        }
+    }
+        
+
+        /*
+        // przykład
+        x=65; //[OK]zmienną możemy zmienić w dowolnym miejscu
+        pi=5555; //[ERROR]  stała możemy zadeklarować tylko raz
+         */
+
+        
